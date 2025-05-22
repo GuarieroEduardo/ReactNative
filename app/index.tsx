29b1c20fdad51e715e74a5b6123ebbcf9c21a3d4
GuarieroEduardo <eduvardo16@gmail.com>
2025-05-21 21:38:47 -0400

    ok

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,20 @@
-import { Text, View } from "react-native";
+import { Image, Text, View, Button, TouchableOpacity, TextInput } from "react-native";
 
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text className="text-5xl text-dark-200 font-bold">Bem vindo</Text>
+      <Image
+        className="rounded-full w-36 h-36 mb-6"
+        source={{uri: "https://th.bing.com/th/id/OIP.MSUT7ZG8wk9gcfxYhznLrwHaJ-?cb=iwc2&rs=1&pid=ImgDetMain",}}
+      />
+      <Text className="text-5xl text-dark-200 font-bold">Mario</Text>
+      <Text className="text-base text-dark-200 font-bold">Idade: 25 anos</Text>
+      <Text className="text-base text-dark-200 font-bold">Localização: Nintendo</Text>
+      
+      <TouchableOpacity className="rounded-full bg-blue-500 p-2 mt-4">
+        <Text className="text-white">Editar Perfil</Text>
+      </TouchableOpacity>
+
     </View>
   );
 }
