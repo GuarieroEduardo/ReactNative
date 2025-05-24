09f0aba4f2098bcbb91ee357e7b418aeca8461fa
GuarieroEduardo <eduvardo16@gmail.com>
2025-05-23 20:47:54 -0400

    exercicio2

--- app/index.tsx
+++ app/index.tsx
@@ -2,17 +2,22 @@ import { Image, Text, View, Button, TouchableOpacity, TextInput } from "react-na
 
 export default function Index() {
   return (
-    <View className="flex-1 justify-center items-center">
-      <Image
-        className="rounded-full w-36 h-36 mb-6"
-        source={{uri: "https://th.bing.com/th/id/OIP.MSUT7ZG8wk9gcfxYhznLrwHaJ-?cb=iwc2&rs=1&pid=ImgDetMain",}}
-      />
-      <Text className="text-5xl text-dark-200 font-bold">Mario</Text>
-      <Text className="text-base text-dark-200 font-bold">Idade: 25 anos</Text>
-      <Text className="text-base text-dark-200 font-bold">Localização: Nintendo</Text>
+    <View>
+      <Text className="bg-blue-500 font-bold text-white pl-2 pr-2 pb-2 pt-2">Detalhe do produto</Text>
+
+      <View className="flex-1 items-center  pt-4">
+        <Image
+          className=" w-56 h-56 mb-6"
+          source={{uri: "https://http2.mlstatic.com/camisa-social-masculina-slim-fit-manga-longa-cor-jeans-D_NQ_NP_636622-MLB31206242433_062019-F.jpg",}}
+        />
+
+      </View>
+      <Text className="text-3xl text-dark-200 font-bold ml-4 mr-4 pb-2 pt-2">Produto Excepcional</Text>
+      <Text className="text-1xl text-dark-200 ml-4 mr-4 pb-2 pt-2">Camisa de algodão confortável e versátil, ideal para qualquer ocasião. Design clássico, fácil de combinar com outras peças do guarda-roupa. Disponível em várias cores e tamanhos, perfeita para um visual elegante e descomplicado.</Text>
+      <Text className="text-2xl text-green-500 font-bold ml-4 mr-4 pb-2 pt-2">R$ 200,00</Text>
       
-      <TouchableOpacity className="rounded-full bg-blue-500 p-2 mt-4">
-        <Text className="text-white">Editar Perfil</Text>
+      <TouchableOpacity className="rounded-full bg-blue-500 p-2 mt-4 ml-4 mr-4 items-center">
+        <Text className="text-white">Comprar agora</Text>
       </TouchableOpacity>
 
     </View>
