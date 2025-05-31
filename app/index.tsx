5aa633d08f0d2bf4971545cc2c05f5e849de51b1
ebuarbin <eduvardo16@gmail.com>
2025-05-31 16:21:07 -0300

    feito

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,40 @@
-import { Image, Text, View, ScrollView, TouchableOpacity } from "react-native";
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
 
-export default function Dashboard() {
+export default function App() {
+  const [tarefa, setTarefa] = useState('');
+  const [lista, setLista] = useState([]);
+
   return (
-    <View className="flex-1 bg-white">
-      
+    <View style={{ padding: 20 }}>
+      <Text>Contador de Tarefas</Text>
+
+      <TextInput
+        placeholder="Digite uma tarefa"
+        value={tarefa}
+        onChangeText={setTarefa}
+        style={{ borderWidth: 1, padding: 8, marginVertical: 10 }}
+      />
+
+      <TouchableOpacity
+          onPress={() => {
+            setLista([...lista, tarefa]);
+            setTarefa('');
+          }}
+          style={{
+            backgroundColor: 'blue',
+            padding: 10,
+            borderRadius: 5
+          }}
+        >
+        <Text style={{ color: 'white' }}>Adicionar</Text>
+      </TouchableOpacity>
+
+      <Text>Total de Tarefas: {lista.length}</Text>
+
+      {lista.map((item, index) => (
+        <Text key={index}>- {item}</Text>
+      ))}
     </View>
   );
 }
